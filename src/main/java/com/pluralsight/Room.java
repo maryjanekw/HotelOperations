58066f66537ebcda09b6d19eea48ce96980c9037
Maryjane Watanabe <maryjanekw@gmail.com>
2025-10-22 10:02:34 -0700

    room variables and getters

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
 public class Room {
+
+    // The Room class is responsible for knowing everything related to a hotel room. A
+    //room is only available if it is clean and not currently occupied.
+
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+    private boolean available;
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
 }
